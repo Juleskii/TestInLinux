1887f16792158f7a555846f4d9d759c4743d7dc3
Juleskii <jules.kenneth@hotmail.com>
2021-10-21 13:50:40 -0400

    Added list of cars

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    // main method should be lowercase
+    // Main will be a constructor
+
+    public static void main(String[] args) {
+
+        // Creates an array of car models
+        String[] carModels = {
+            "Acura", 
+            "Alfa Romeo", 
+            "Audi", 
+            "BMW", 
+            "Buick", 
+            "Cadillac", 
+            "Chevrolet", 
+            "Fiat", 
+            "Kia", 
+            "Jeep", 
+            "Lincoln", 
+            "Lotus", 
+            "Porsche", 
+        };
+        
+        for (String i : carModels) {
+            System.out.println(i);
+        } 
+    }
+  }
